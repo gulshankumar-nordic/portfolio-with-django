dffa372523475bb581e596af2610bb96706fc657
Gulshan <honeykulta@gmail.com>
2019-01-13 23:32:08 +0200

    Created views for index, portfolio, projects and contact

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "portfolio/index.html")
+
+def portfolio(request):
+    return render(request, "portfolio/portfolio.html")
+
+def projects(request):
+    return render(request, "portfolio/projects.html")
+
+def contact(request):
+    return render(request, "portfolio/portfolio.html")
